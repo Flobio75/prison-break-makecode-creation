02d6098a4ed0b900da5b798b1f6adf9b2e0220a4
Flobio75 <105999091+Flobio75@users.noreply.github.com>
2024-02-03 11:28:32 -0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -206,11 +206,8 @@ function initPlayfield () {
     playerSprite.setPosition(scene.screenWidth() / 2, scene.screenHeight() - 36)
     playerSprite.setStayInScreen(true)
     buckets.push(playerSprite)
-    console.logValue("x", MAX_BUCKETS - 2)
     for (let index = 0; index <= MAX_BUCKETS - 2; index++) {
-        console.logValue("x", MAX_BUCKETS - 2)
         addBucket(index)
-        console.logValue("buckets=", buckets.length)
     }
 }
 function extraLife () {
@@ -244,14 +241,10 @@ function updateGame () {
     checkBuckets()
     if (gameState == 0) {
         if (bombsDropped < bombCount) {
-            timer.throttle("BombDrop", dropInterval, function () {
-                dropBomb()
-                timer.background(function () {
-                    timer.throttle("changeDirection", directionChange, function () {
-                        changeBaddyDirection()
-                        directionChange = randint(directionChangeLB, directionChangeUB)
-                    })
-                })
+            dropBomb()
+            timer.throttle("changeDirection", directionChange, function () {
+                changeBaddyDirection()
+                directionChange = randint(directionChangeLB, directionChangeUB)
             })
         } else {
             gameState = 2
